4e3e28675cc43385da408b97fc0756f1a35331b9
tah167 <43456433+tah167@users.noreply.github.com>
2018-10-26 16:35:02 -0400

    Update generator.java

--- generator.java
+++ generator.java
@@ -1,4 +1,6 @@
 
+
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.io.File;
@@ -33,7 +35,12 @@ public class generator {
 				"The Bar", "The Moon Under Water", "The Bang Bang Bar", "Cheers", "Bada Bing!", "Greely's", "Chatsubo", "Bob's Country Bunker", 
 				"Club Sugar Ray", "The Hog's Head Inn", "The Last Resort", "The Tiny Twister", "Dino's Bar and Grill", "Winchester Tavern",
 				"A Clean, Well-Lighted Place", "The Gold Room", "Paddy's Pub", "Boar's Head Tavern", "The Gem Saloon", "Korova Milk Bar",
-				"Mos Eisley Cantina", "Rick's Cafe Americanain", "Moe's Tavern"};
+				"Mos Eisley Cantina", "Rick's Cafe Americanain", "Moe's Tavern", "Salty Squid", "Inky Stink" "Leaky Faucet", "Rusted Furnace", "Mystic Island Casino",
+				"Tucker's Tavern", "Mickey's Pub", "Weenie Hut Jr's", "Bean Bag", "Lighthouse Bar and Grill", "Burnt Toast", "Lefty's Pub", "Element", "Oyster Creek Inn",
+				"Scarlet Pub", "Clydz", "Corner Tavern", "Barca City", "The Dogg House", "Steve Vo's", "Caribbean Breeze", "Cajun Cabin", "Jazzy B's", "Coffeecake", "Street Dips",
+				"Just Off Vine", "El Tenedor", "Wilma's", "Jimmy D's", "Jalapeno Brothers", "Off the Hook", "The Rolling Hunger", "Fast Eddie's", "Bernie's Burger Bus", "Mr. Sizzles", "1836 Grill", "Burgers Ya Heard", "The Boxcar", "The Store", "Lil Phil's Grill", "Victory",
+				"The Hideout Theatre", "Midnight Cowboy", "The Roosevelt Room", "Firehouse Lounge", "HandleBar", "Garage", "Friends Bar", "Whisler's", "Dive Bar and Lounge", "Seven Grand", "Craft Pride", "Knomad Bar", "The Townsend", "The Jackalope", "Half Step", "Full Circle Bar", "The Skylark Lounge",
+				"The Crazy Horse", "Mikkeller Bar", "Bourbon and Branch", "Smuggler's Cove", "Pacific Cocktail Haven", "Benjamin Cooper", "Rickhouse", "Holy Water", "Trick Dog", "Comstock Saloon", "Whitechapel", "Black Horse London Pub", "15 Romolo", "Novela", "Forgery Bar", "Cityscape", "Local Edition", "Bar Fluxus", "Zeitgeist", "Tonga Room", "Top of the Mark", "Fireside Bar", "Library Bar", "Last Call Bar", "The Alembic", "Kozy Kar", "Rock Bar", "Zam Zam", "Toronado", "Bar 821", "The House of shields", "Buddha Lounge", "Wildhawk", "Tempest", "BlackBird", "Third Rail", "Cold Drinks Bar", "Madrone Art Bar", "Clock Bar", "Aunt Charlie's Lounge", "Linden Room", "Bender's", "The Page", "The Starlight Room", "The Monk's Kettle", "Bond Bar", "Dalva", "White Cap", "Canela Bistro and Wine Bar", "Bus Stop", "Press Club", "Hazel Southern Bar", "Monaghan's Bar", "Noc Noc", "Oddjob", "The Sea Star", "Resolute", "The Beer Hall", "Hi Tops", "Union Square Sports Bar", "Blondie's Bar", "Brass Tracks", "Bar San Pancho", "The Pied Piper", "Charmaine's", "The Chapel"};
 		String[] location = {"Connecticut", "Delaware", "Massachusetts", "Maryland", "New Hampshire", "Pennsylvania", "New Jersey", "New York", 
 				"Virginia", "Tennessee", "Florida", "Georgia", "Ohio", "Iowa", "Mississippi", "Lousiana", "Maine", "Rhode Island", "Vermont", 
 				"Alabama", "Kentucky", "North Carolina", "South Carolina", "West Virginia"};
@@ -105,8 +112,10 @@ public class generator {
 	}
 	
 	public static ArrayList<String> items() {
-		String[] beers = {"Budweiser,Anheuser-Busch", "Stella Artios,Anheuser-Busch", "Landshark Lager,Anheuser-Busch", "Hoegaarden,Anheuser-Busch", "Coors,MillerCoors", "Icehouse,MillerCoors", "Blue Moon,MillerCoors" , "Pilsner,MillerCoors", "Heineken,Heineken USA", "Dos Equis,Heineken USA", "Corona,Constellation Brands", "Tsingtao,Constellation Brands", "Pabst Blue Ribbon,Pabst", "Lone Star,Pabst", "Samuel Adams,Boston Beer Co.", "Guinness,Diageo North America"};
-		String[] other = {"Hamburger,NULL", "Frie,NULLs", "Hot Dog,NULL", "Lemonade,NULL", "Coke,NULL", "Mountain Dew,NULL", "Pretzels,NULL", "Goldfish,NULL", "Coffee,NULL", "Soup,NULL", "Wings,NULL", "Nachos,NULL", "French Dip,NULL", "Grilled Cheese,NULL", "Fat Sandwich,NULL"};
+		String[] beers = {"Budweiser,Anheuser-Busch", "Stella Artios,Anheuser-Busch", "Landshark Lager,Anheuser-Busch", "Hoegaarden,Anheuser-Busch", "Coors,MillerCoors", "Icehouse,MillerCoors", "Blue Moon,MillerCoors" , "Pilsner,MillerCoors", "Heineken,Heineken USA", "Dos Equis,Heineken USA", "Corona,Constellation Brands", "Tsingtao,Constellation Brands", "Pabst Blue Ribbon,Pabst", "Lone Star,Pabst", "Samuel Adams,Boston Beer Co.", "Guinness,Diageo North America", "Fat Tire,New Belgium", "Miller Light,Miller Brewing", "Red Stripe,Desnoes & Geddes", "Celis White,Pierre Celis"
+		"Dale's Pale Ale,Oskar Blues", "Vanilla Porter,Breckenridge", "Brooklyn Brewery Lager,Brooklyn Brewery", "Surly Brewing Darkness,Surly", "Dogfish Head,Dogfish Head", "Pipeworks Citra,Pipeworks", "Hefeweizen,Widmer Brother", "Saison Rue,The Bruery", "Uncle Jacob's Stout,Avery Brewing", "Focal Banger,The Alchemist", "Abner,Hill Farmstead", "Gose,Westbrook", "Union Jack,Firestone Walker",
+		"Cold Mountain Winter Ale,Highland Brewing", "Allagash White,Allagash", "Anchor Steam Beer,Anchor Brewing", "Craftsman Cabernale, Craftsman Brewing", "Bell's Two Hearted,Bell's Brweing", "Honry Devil,Alesmith Brewing", "Bitter American,21st Amendment Brewery", "Nugget Nectar, Troegs Brewing", "Upslope Brown Ale,Upslope Brewing", "Shakespeare Oatmeal Stout,Rogue Ales and Spirits", "Fancy Lawnmower,Saint Arnold Brewing", "Schnickelfritz, Urban Chestnut", "Fullsteam Carver,Fullsteam Brewery", "Hop Head Red,Green Flash Brewing", "Blind Pig IPA,Russian River Brewing", "Hop Slam, Bell's Brewery", "La Roja,Jolly Pumpkin Artisan Ales", "PseudoSue,Toppling Goliath"};
+		String[] other = {"Hamburger,NULL", "Fries,NULLs", "Hot Dog,NULL", "Lemonade,NULL", "Coke,NULL", "Mountain Dew,NULL", "Pretzels,NULL", "Goldfish,NULL", "Coffee,NULL", "Soup,NULL", "Wings,NULL", "Nachos,NULL", "French Dip,NULL", "Grilled Cheese,NULL", "Fat Sandwich,NULL"};
 		
 		ArrayList<String> Items = new ArrayList<String>();
 		for(int i = 0; i < beers.length; i++) {

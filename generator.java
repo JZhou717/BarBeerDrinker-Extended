17ddab94d05f65e8c87051ff8331be194652d542
JZhou717 <32233263+JZhou717@users.noreply.github.com>
2018-10-27 15:45:13 -0400

    Updated generator code
    This update includes pulling from a txt file of 5000+ first names, as well as some other data that we are not using. This also includes some more updates on Bars, Beers, and Items lists in the java file

--- generator.java
+++ generator.java
@@ -1,6 +1,4 @@
-
-
-
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 import java.io.File;
@@ -11,9 +9,12 @@ public class generator {
 /*
  * We are going to be generating random data for BarBeerDrinker database	
  */
-	public static void main(String[] args) throws FileNotFoundException{
-		BarsCSV(bars());
-		DrinkersCSV(drinkers());
+	public static void main(String[] args) throws Exception{
+		
+		//ArrayList<String> names = nameGenerator();
+		
+		//BarsCSV(bars());
+		//DrinkersCSV(drinkers(names));
 		ItemsCSV(items());
 		//bars();
 		//drinkers();
@@ -35,12 +36,23 @@ public class generator {
 				"The Bar", "The Moon Under Water", "The Bang Bang Bar", "Cheers", "Bada Bing!", "Greely's", "Chatsubo", "Bob's Country Bunker", 
 				"Club Sugar Ray", "The Hog's Head Inn", "The Last Resort", "The Tiny Twister", "Dino's Bar and Grill", "Winchester Tavern",
 				"A Clean, Well-Lighted Place", "The Gold Room", "Paddy's Pub", "Boar's Head Tavern", "The Gem Saloon", "Korova Milk Bar",
-				"Mos Eisley Cantina", "Rick's Cafe Americanain", "Moe's Tavern", "Salty Squid", "Inky Stink" "Leaky Faucet", "Rusted Furnace", "Mystic Island Casino",
-				"Tucker's Tavern", "Mickey's Pub", "Weenie Hut Jr's", "Bean Bag", "Lighthouse Bar and Grill", "Burnt Toast", "Lefty's Pub", "Element", "Oyster Creek Inn",
-				"Scarlet Pub", "Clydz", "Corner Tavern", "Barca City", "The Dogg House", "Steve Vo's", "Caribbean Breeze", "Cajun Cabin", "Jazzy B's", "Coffeecake", "Street Dips",
-				"Just Off Vine", "El Tenedor", "Wilma's", "Jimmy D's", "Jalapeno Brothers", "Off the Hook", "The Rolling Hunger", "Fast Eddie's", "Bernie's Burger Bus", "Mr. Sizzles", "1836 Grill", "Burgers Ya Heard", "The Boxcar", "The Store", "Lil Phil's Grill", "Victory",
-				"The Hideout Theatre", "Midnight Cowboy", "The Roosevelt Room", "Firehouse Lounge", "HandleBar", "Garage", "Friends Bar", "Whisler's", "Dive Bar and Lounge", "Seven Grand", "Craft Pride", "Knomad Bar", "The Townsend", "The Jackalope", "Half Step", "Full Circle Bar", "The Skylark Lounge",
-				"The Crazy Horse", "Mikkeller Bar", "Bourbon and Branch", "Smuggler's Cove", "Pacific Cocktail Haven", "Benjamin Cooper", "Rickhouse", "Holy Water", "Trick Dog", "Comstock Saloon", "Whitechapel", "Black Horse London Pub", "15 Romolo", "Novela", "Forgery Bar", "Cityscape", "Local Edition", "Bar Fluxus", "Zeitgeist", "Tonga Room", "Top of the Mark", "Fireside Bar", "Library Bar", "Last Call Bar", "The Alembic", "Kozy Kar", "Rock Bar", "Zam Zam", "Toronado", "Bar 821", "The House of shields", "Buddha Lounge", "Wildhawk", "Tempest", "BlackBird", "Third Rail", "Cold Drinks Bar", "Madrone Art Bar", "Clock Bar", "Aunt Charlie's Lounge", "Linden Room", "Bender's", "The Page", "The Starlight Room", "The Monk's Kettle", "Bond Bar", "Dalva", "White Cap", "Canela Bistro and Wine Bar", "Bus Stop", "Press Club", "Hazel Southern Bar", "Monaghan's Bar", "Noc Noc", "Oddjob", "The Sea Star", "Resolute", "The Beer Hall", "Hi Tops", "Union Square Sports Bar", "Blondie's Bar", "Brass Tracks", "Bar San Pancho", "The Pied Piper", "Charmaine's", "The Chapel"};
+				"Mos Eisley Cantina", "Rick's Cafe Americanain", "Moe's Tavern", "Salty Squid", "Inky Stink", "Leaky Faucet", "Rusted Furnace", 
+				"Mystic Island Casino", "Tucker's Tavern", "Mickey's Pub", "Weenie Hut Jr's", "Bean Bag", "Lighthouse Bar and Grill", "Burnt Toast", 
+				"Lefty's Pub", "Element", "Oyster Creek Inn", "Scarlet Pub", "Clydz", "Corner Tavern", "Barca City", "The Dogg House", "Steve Vo's", 
+				"Caribbean Breeze", "Cajun Cabin", "Jazzy B's", "Coffeecake", "Street Dips", "Just Off Vine", "El Tenedor", "Wilma's", "Jimmy D's", 
+				"Jalapeno Brothers", "Off the Hook", "The Rolling Hunger", "Fast Eddie's", "Bernie's Burger Bus", "Mr. Sizzles", "1836 Grill", 
+				"Burgers Ya Heard", "The Boxcar", "The Store", "Lil Phil's Grill", "Victory", "The Hideout Theatre", "Midnight Cowboy", 
+				"The Roosevelt Room", "Firehouse Lounge", "HandleBar", "Garage", "Friends Bar", "Whisler's", "Dive Bar and Lounge", "Seven Grand", 
+				"Craft Pride", "Knomad Bar", "The Townsend", "The Jackalope", "Half Step", "Full Circle Bar", "The Skylark Lounge",
+				"The Crazy Horse", "Mikkeller Bar", "Bourbon and Branch", "Smuggler's Cove", "Pacific Cocktail Haven", "Benjamin Cooper", 
+				"Rickhouse", "Holy Water", "Trick Dog", "Comstock Saloon", "Whitechapel", "Black Horse London Pub", "15 Romolo", "Novela", 
+				"Forgery Bar", "Cityscape", "Local Edition", "Bar Fluxus", "Zeitgeist", "Tonga Room", "Top of the Mark", "Fireside Bar", 
+				"Library Bar", "Last Call Bar", "The Alembic", "Kozy Kar", "Rock Bar", "Zam Zam", "Toronado", "Bar 821", "The House of shields", 
+				"Buddha Lounge", "Wildhawk", "Tempest", "BlackBird", "Third Rail", "Cold Drinks Bar", "Madrone Art Bar", "Clock Bar", 
+				"Aunt Charlie's Lounge", "Linden Room", "Bender's", "The Page", "The Starlight Room", "The Monk's Kettle", "Bond Bar", 
+				"Dalva", "White Cap", "Canela Bistro and Wine Bar", "Bus Stop", "Press Club", "Hazel Southern Bar", "Monaghan's Bar", 
+				"Noc Noc", "Oddjob", "The Sea Star", "Resolute", "The Beer Hall", "Hi Tops", "Union Square Sports Bar", "Blondie's Bar", 
+				"Brass Tracks", "Bar San Pancho", "The Pied Piper", "Charmaine's", "The Chapel"};
 		String[] location = {"Connecticut", "Delaware", "Massachusetts", "Maryland", "New Hampshire", "Pennsylvania", "New Jersey", "New York", 
 				"Virginia", "Tennessee", "Florida", "Georgia", "Ohio", "Iowa", "Mississippi", "Lousiana", "Maine", "Rhode Island", "Vermont", 
 				"Alabama", "Kentucky", "North Carolina", "South Carolina", "West Virginia"};
@@ -54,16 +66,16 @@ public class generator {
 			int crnd = new Random().nextInt(closing_hours.length);
 			Bars.add(bars[i] + "," + location[lrnd] + "," + opening_hours[ornd] + "," + closing_hours[crnd]);
 		}
-		for(int i = 0; i < Bars.size(); i++) {
+		/*for(int i = 0; i < Bars.size(); i++) {
 			System.out.println(Bars.get(i));
-		}
+		}*/
 		
 		return Bars;
 	}
 	
 	public static void BarsCSV(ArrayList<String> Bars) throws FileNotFoundException{
 		PrintWriter pw = new PrintWriter(new File("c:/Users/Jake/Desktop/School/Fall 2018/Data Management/Project/Bars.csv"));
-		String header = "name,location,opening_hours,closing_hours" + '\n';
+		String header = "name,location,opening_hours,closing_hours";
 		pw.println(header);
 		for(int i = 0; i < Bars.size(); i++) {
 			pw.println((Bars.get(i)).toString());
@@ -71,39 +83,50 @@ public class generator {
 		pw.close();
 	}
 	
-	public static ArrayList<String> drinkers() {
-		String[] names = {"Tom","Jake", "Mike", "Carl", "Bob","Joe",
-				"Shreya", "Olivia", "Nick", "Ally", "Carly", "Nicole", "Susie",
-				"Lily", "Kelly", "Leah", "Lori", "Ryan", "Sarah", "Marley",
-				"Jackie", "Abby", "Adam", "AJ", "Akhil", "Amanda", "Ashley", "Austin",
-				"Bill", "Brad", "Briana", "Brielle", "Mary", "Chad", "Chris", "Colleen",
-				"Danielle", "Deep", "Emmet", "Erica", "Jessica", "Jeff", "Geoff", "George", "Juan", "Griffin",
-				"Nicar", "Gustavo", "Sasha", "Hannah", "Hunter", "Harry", "Jason", "Jessie", "John", "Jose", "Julian",
-				"Kyle", "Karen", "Liz", "Matt", "Max", "Megan", "Molly", "Nina", "Natalie", "Oliver", "Srid", "Sidd", "Roland",
-				"Rosie", "Sage", "Sahara", "Samia", "Sean", "Shawn", "Sue", "Alex", "Shannon", "Kristina", "Caroline", "Terry",
-				"Ted", "Tyler", "Wesley", "Xena", "Yash", "Lee", "Bingchen", "Jamar", "Tyrone", "Doran", "David", "William", "Victoria",
-				"Michelle", "Myron", "Snoop"};
+	//This function reads the txt file of names that we found and extracts just the name from each line
+	public static ArrayList<String> nameGenerator() throws Exception{
+		ArrayList<String> list = new ArrayList<String>();
+		File file = new File("C:/Users/Jake/myWorkspace/databases/src/nameList.txt");
+		Scanner sc = new Scanner(file);
+		String buffer;
+		
+		for(int i = 0; i < 1500; i++) {
+			buffer = sc.nextLine();
+			String[] line = buffer.split("\\s+", 2);
+			String name = line[0];
+			
+			//System.out.println(name);
+			name = name.charAt(0) + name.substring(1).toLowerCase();
+			//System.out.println(name);
+			
+			list.add(name);
+		}
+		sc.close();
+		return list;
+	}
+	
+	public static ArrayList<String> drinkers(ArrayList<String> names) {
 
 		String[] address = {"Connecticut", "Delaware", "Massachusetts", "Maryland", "New Hampshire", "Pennsylvania", "New Jersey", "New York", 
 				"Virginia", "Tennessee", "Florida", "Georgia", "Ohio", "Iowa", "Mississippi", "Lousiana", "Maine", "Rhode Island", "Vermont", 
 				"Alabama", "Kentucky", "North Carolina", "South Carolina", "West Virginia"};
 		
 		ArrayList<String> Drinkers = new ArrayList<String>();
-		for(int i = 0; i < names.length; i++) {
+		for(int i = 0; i < names.size(); i++) {
 			int rnd = new Random().nextInt(address.length);
-			Drinkers.add(names[i] + "," + address[rnd]);
+			Drinkers.add(names.get(i) + "," + address[rnd]);
 		}
 		
-		for(int i = 0; i < Drinkers.size(); i++) {
+		/*for(int i = 0; i < Drinkers.size(); i++) {
 			System.out.println(Drinkers.get(i));
-		}
+		}*/
 		
 		return Drinkers;
 	}
 	
 	public static void DrinkersCSV(ArrayList<String> Drinkers) throws FileNotFoundException{
 		PrintWriter pw = new PrintWriter(new File("c:/Users/Jake/Desktop/School/Fall 2018/Data Management/Project/Drinkers.csv"));
-		String header = "name,address" + '\n';
+		String header = "name,address";
 		pw.println(header);
 		for(int i = 0; i < Drinkers.size(); i++) {
 			pw.println((Drinkers.get(i)).toString());
@@ -112,30 +135,42 @@ public class generator {
 	}
 	
 	public static ArrayList<String> items() {
-		String[] beers = {"Budweiser,Anheuser-Busch", "Stella Artios,Anheuser-Busch", "Landshark Lager,Anheuser-Busch", "Hoegaarden,Anheuser-Busch", "Coors,MillerCoors", "Icehouse,MillerCoors", "Blue Moon,MillerCoors" , "Pilsner,MillerCoors", "Heineken,Heineken USA", "Dos Equis,Heineken USA", "Corona,Constellation Brands", "Tsingtao,Constellation Brands", "Pabst Blue Ribbon,Pabst", "Lone Star,Pabst", "Samuel Adams,Boston Beer Co.", "Guinness,Diageo North America", "Fat Tire,New Belgium", "Miller Light,Miller Brewing", "Red Stripe,Desnoes & Geddes", "Celis White,Pierre Celis"
-		"Dale's Pale Ale,Oskar Blues", "Vanilla Porter,Breckenridge", "Brooklyn Brewery Lager,Brooklyn Brewery", "Surly Brewing Darkness,Surly", "Dogfish Head,Dogfish Head", "Pipeworks Citra,Pipeworks", "Hefeweizen,Widmer Brother", "Saison Rue,The Bruery", "Uncle Jacob's Stout,Avery Brewing", "Focal Banger,The Alchemist", "Abner,Hill Farmstead", "Gose,Westbrook", "Union Jack,Firestone Walker",
-		"Cold Mountain Winter Ale,Highland Brewing", "Allagash White,Allagash", "Anchor Steam Beer,Anchor Brewing", "Craftsman Cabernale, Craftsman Brewing", "Bell's Two Hearted,Bell's Brweing", "Honry Devil,Alesmith Brewing", "Bitter American,21st Amendment Brewery", "Nugget Nectar, Troegs Brewing", "Upslope Brown Ale,Upslope Brewing", "Shakespeare Oatmeal Stout,Rogue Ales and Spirits", "Fancy Lawnmower,Saint Arnold Brewing", "Schnickelfritz, Urban Chestnut", "Fullsteam Carver,Fullsteam Brewery", "Hop Head Red,Green Flash Brewing", "Blind Pig IPA,Russian River Brewing", "Hop Slam, Bell's Brewery", "La Roja,Jolly Pumpkin Artisan Ales", "PseudoSue,Toppling Goliath"};
-		String[] other = {"Hamburger,NULL", "Fries,NULLs", "Hot Dog,NULL", "Lemonade,NULL", "Coke,NULL", "Mountain Dew,NULL", "Pretzels,NULL", "Goldfish,NULL", "Coffee,NULL", "Soup,NULL", "Wings,NULL", "Nachos,NULL", "French Dip,NULL", "Grilled Cheese,NULL", "Fat Sandwich,NULL",
-		"Buffalo Chicken Sandwich", "Cheesesteak", "Mac n Cheese", "Dumplings", "Key Lime Pie", "Cheesecake", "Chocolate Cake", "Spaghetti", "Penne Vodka", "Calamari", "Pancakes", "Waffle", "Bacon, Egg and Cheese", "Lobster", "Crab"};
+		String[] beers = {"Budweiser,Anheuser-Busch", "Stella Artios,Anheuser-Busch", "Landshark Lager,Anheuser-Busch", "Hoegaarden,Anheuser-Busch", 
+				"Coors,MillerCoors", "Icehouse,MillerCoors", "Blue Moon,MillerCoors" , "Pilsner,MillerCoors", "Heineken,Heineken USA", 
+				"Dos Equis,Heineken USA", "Corona,Constellation Brands", "Tsingtao,Constellation Brands", "Pabst Blue Ribbon,Pabst", 
+				"Lone Star,Pabst", "Samuel Adams,Boston Beer Co.", "Guinness,Diageo North America", "Fat Tire,New Belgium", "Miller Light,Miller Brewing", 
+				"Red Stripe,Desnoes & Geddes", "Celis White,Pierre Celis", "Dale's Pale Ale,Oskar Blues", "Vanilla Porter,Breckenridge", 
+				"Brooklyn Brewery Lager,Brooklyn Brewery", "Surly Brewing Darkness,Surly", "Dogfish Head,Dogfish Head", "Pipeworks Citra,Pipeworks", 
+				"Hefeweizen,Widmer Brother", "Saison Rue,The Bruery", "Uncle Jacob's Stout,Avery Brewing", "Focal Banger,The Alchemist", "Abner,Hill Farmstead", "Gose,Westbrook", "Union Jack,Firestone Walker",
+				"Cold Mountain Winter Ale,Highland Brewing", "Allagash White,Allagash", "Anchor Steam Beer,Anchor Brewing", 
+				"Craftsman Cabernale,Craftsman Brewing", "Bell's Two Hearted,Bell's Brewing", "Honry Devil,Alesmith Brewing", 
+				"Bitter American,21st Amendment Brewery", "Nugget Nectar,Troegs Brewing", "Upslope Brown Ale,Upslope Brewing", 
+				"Shakespeare Oatmeal Stout,Rogue Ales and Spirits", "Fancy Lawnmower,Saint Arnold Brewing", "Schnickelfritz,Urban Chestnut", 
+				"Fullsteam Carver,Fullsteam Brewery", "Hop Head Red,Green Flash Brewing", "Blind Pig IPA,Russian River Brewing", 
+				"Hop Slam, Bell's Brewery", "La Roja,Jolly Pumpkin Artisan Ales", "PseudoSue,Toppling Goliath"};
+		String[] other = {"Hamburger", "Frie", "Hot Dog", "Lemonade", "Coke", "Mountain Dew", "Pretzels",
+				"Goldfish", "Coffee", "Soup", "Wings", "Nachos", "French Dip", "Grilled Cheese", "Fat Sandwich",
+				"Buffalo Chicken Sandwich", "Cheesesteak", "Mac n Cheese", "Dumplings", "Key Lime Pie", "Cheesecake", "Chocolate Cake", "Spaghetti",
+				"Penne Vodka", "Calamari", "Pancakes", "Waffle", "Bacon Egg and Cheese", "Lobster", "Crab"};
 		
 		ArrayList<String> Items = new ArrayList<String>();
 		for(int i = 0; i < beers.length; i++) {
 			Items.add(beers[i]);
 		}
 		for(int j = 0; j < other.length; j++) {
-			Items.add(other[j]);
+			Items.add(other[j] + ",NULL");
 		}
 		
-		for(int i = 0; i < Items.size(); i++) {
+		/*for(int i = 0; i < Items.size(); i++) {
 			System.out.println(Items.get(i));
-		}
+		}*/
 		
 		return Items;
 	}
 	
 	public static void ItemsCSV(ArrayList<String> Items) throws FileNotFoundException{
 		PrintWriter pw = new PrintWriter(new File("c:/Users/Jake/Desktop/School/Fall 2018/Data Management/Project/Items.csv"));
-		String header = "name,manufacturer" + '\n';
+		String header = "name,manufacturer";
 		pw.println(header);
 		for(int i = 0; i < Items.size(); i++) {
 			pw.println((Items.get(i)).toString());
